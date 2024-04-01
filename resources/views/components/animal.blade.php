f6aa710300b140ac44a9366fb5928be2ce26942f
NNT9x <42613109+nnt9x@users.noreply.github.com>
2024-04-01 08:50:48 +0700

    Initial commit

---
+++ resources/views/components/animal.blade.php
@@ -0,0 +1,7 @@
+<div class="card" style="width: 18rem;">
+    <img src="{{$image}}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">{{$name}}</h5>
+        <p class="card-text">{{$description}}</p>
+    </div>
+</div>
